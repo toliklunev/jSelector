042bdd947f7bc46feba8a7c0bbe5d6e82210f0f2
Anatoly <toliklunev@ya.ru>
2013-06-15 15:22:45 +0400

    update

--- source/jquery.jselector.js
+++ source/jquery.jselector.js
@@ -1,50 +1,77 @@
 // todo опция устанавливать ширину
 
 ;(function($){
-	$.fn.jselector = function(options){
-		this.each(function(){
+	$.fn.jselector = function(arg){
 
-			var $select   = $(this);
-			var $selector = $select.wrap('<div class="jselector">').parent();
-			var $ul       = $('<ul>').hide().appendTo($selector);
-			var $span     = $('<span>').prependTo($selector);
-			var $dropdown = $('<button type="button" class="dropdown">').text('▼').appendTo($selector);
-
-			$('option', $select).each(function(i){
-
-				var $option = $(this);
-				var $item = $('<li>');
-				var text = $option.text();
-
-				$ul.append($item.text(text));
-
-				if($option.is(':selected')){
-					$item.addClass('selected');
-					$span.text(text);
+		function make(){
+			if(typeof arg === 'string'){
+				switch(arg){
+					case 'update':
+						$(this).trigger('jselector.update');
+						return false;
+						break;
 				}
+			}
+
+			var $select        = $(this);
+			var $selector      = $select.wrap('<div class="jselector">').parent();
+			var $selector_wrap = $('<div class="wrap">').appendTo($selector);
+			var $ul            = $('<ul>').hide().appendTo($selector);
+			var $span          = $('<span>').prependTo($selector_wrap);
+			var $dropdown      = $('<button type="button" class="dropdown">').text('▼').appendTo($selector_wrap);
+
+			$select.bind('jselector.update', function(){
+
+				$ul.empty();
+
+				$('option', $select).each(function(){
+					var $option = $(this);
+					var $item = $('<li>');
+					var text = $option.text();
+
+					$ul.append($item.text(text));
+
+					if($option.is(':selected')){
+						$item.addClass('selected');
+						$span.text(text);
+					}
+				});
+
+				$('li', $ul)
+				.unbind('click.jselector')
+				.bind('click.jselector', function(){
+					$('option', $select).eq($(this).index()).prop('selected', true);
+					$select.change();
+				});
 			});
 
-			$('li', $ul).click(function(){
+			$select.trigger('jselector.update');
 
-				var $item = $(this);
 
-				$('option', $select).eq($item.index()).attr('selected', true);
-				$item.addClass('selected').siblings().removeClass('selected');
+			$select.change(function(){
+				var i = $('option:selected', this).index();
+
+				$item = $('li', $ul).removeClass('selected').eq(i);
+
+				$item.addClass('selected');
 				$span.text($item.text());
 			});
 
-			$selector.click(function(){
+			$selector_wrap.click(function(e){
 				if(!$ul.is(':visible')){
 					$ul.show();
 
+					$(document).click();
+					
 					setTimeout(function(){
-						$(document).bind('click.selector', function(){
-							$(document).unbind('click.selector');
+						$(document).one('click.jselector', function(){
 							$ul.hide();
 						});
-					}, 1)
+					});
 				}
 			});
-		});
+		};
+
+		return this.each(make);
 	}
 })(jQuery);
