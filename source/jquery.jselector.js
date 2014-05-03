9a3747c6ba1fd5be25161934393d24df0338c5bd
Anatoly <toliklunev@ya.ru>
2014-05-04 01:36:02 +0400

    update

--- source/jquery.jselector.js
+++ source/jquery.jselector.js
@@ -1,73 +1,232 @@
-// todo опция устанавливать ширину
+/* jSelector v0.1 | http://codefucker.github.io/jSelector/
+ * © Anatoly Lunev | toliklunev.ru | toliklunev@gmail.com
+ * Licensed under the MIT License */
 
 ;(function($){
-	$.fn.jselector = function(arg){
+	var $document = $(document);
+	var $html;
+
+	var mobile = (/mobile|android|blackberry|brew|htc|j2me|lg|midp|mot|netfront|nokia|obigo|openweb|opera\ mini|palm|psp|samsung|sanyo|sch|sonyericsson|symbian|symbos|teleca|up\.browser|wap|webos|windows\ ce/i.test(navigator.userAgent.toLowerCase()));
+
+	var configuration = {
+		//set_width : true
+	};
+
+	$(function(){
+		$html = $('html');
+
+		if(!mobile){
+			$html.addClass('desktop');
+		}
+	});
+
+	$.fn.jselector = $.fn.jSelector = function(arg){
+
+		if(typeof arg === 'object'){
+			configuration = $.extend(configuration, arg);
+		}
+
+		else if(typeof arg === 'string'){
+			switch(arg){
+				case 'update':
+					this.trigger('jselector:update').change();
+					return;
+					break;
+
+				case 'set_width':
+					this.trigger('jselector:set_width');
+					return;
+					break;
+
+				case 'unset_width':
+					this.trigger('jselector:unset_width');
+					return;
+					break;
+			}
+		}
 
 		function make(){
-			if(typeof arg === 'string'){
-				switch(arg){
-					case 'update':
-						$(this).trigger('jselector.update');
-						return false;
-						break;
-				}
-			}
 
-			var $select        = $(this);
-			var $selector      = $select.wrap('<div class="jselector">').parent();
-			var $selector_wrap = $('<div class="wrap">').appendTo($selector);
-			var $ul            = $('<ul>').hide().appendTo($selector);
-			var $span          = $('<span>').prependTo($selector_wrap);
-			var $dropdown      = $('<button type="button" class="dropdown">').text('▼').appendTo($selector_wrap);
+			var $select   = $(this);
+			var $selector = $select.wrap('<div class="jselector">').parent();
+			var $wrap     = $('<div class="wrap">').appendTo($selector);
+			var $div      = $('<div>').prependTo($wrap);
+			var $span     = $('<span>').prependTo($div);
+			var $button   = $('<button type="button">').text('▼').appendTo($wrap);
+			var $dropdown = $('<div class="dropdown">').appendTo($selector);
+			
+			var $items,
+				$selected_item,
+				$selected_option,
+				$options,
+				$optgroups;
 
-			$select.bind('jselector.update', function(){
+			var $parent;
 
-				$ul.empty();
+			var shown = false;
 
-				$('option', $select).each(function(){
+			var new_items = function(){
+
+				var $ul = $('<ul>').appendTo($parent);
+
+				$('option', this).each(function(){
 					var $option = $(this);
 					var $item = $('<li>');
-					var text = $option.text();
 
-					$ul.append($item.text(text));
+					var text = $option.html();
 
 					if($option.is(':selected')){
-						$item.addClass('selected');
-						$span.text(text);
+						$selected_item = $item.addClass('selected');
+						$selected_option = $option;
+					}
+
+					if($option.is(':disabled')){
+						$item.addClass('disabled').data('disabled', true);
+					}
+
+					$ul.append($item.html(text.replace(/#%/g, '<').replace(/%#/g, '>')));
+				});
+			};
+
+			$selector.attr('data-name', $select.prop('name'));
+
+			$select.bind('jselector:hide', function(){
+				shown = false; 
+				$dropdown.removeClass('shown');
+			});
+
+			$select.bind('jselector:show', function(){
+				shown = true;
+				$dropdown.addClass('shown');
+			});
+
+			$select.bind('jselector:unset_width', function(){
+				$div.css('min-width', '');
+				configuration.set_width = false;
+			});
+
+			// устанавливает минимальную ширину
+			$select.bind('jselector:set_width', function(){
+				var width = 0;
+
+				$div.css('min-width', '');
+
+				$items.each(function(){
+					$span.html($(this).html());
+
+					if($div.width() > width){
+						width = $div.width();
 					}
 				});
 
-				$('li', $ul)
-				.unbind('click.jselector')
-				.bind('click.jselector', function(){
-					$('option', $select).eq($(this).index()).prop('selected', true);
-					$select.change();
-				});
+				if(width){
+					$div.css('min-width', width);
+				}
+
+				$span.html($selected_item.html());
+
+				configuration.set_width = true;
 			});
 
-			$select.trigger('jselector.update');
+			$select.bind('jselector:update', function(){
 
+				$dropdown.empty();
+
+				$optgroups = $('optgroup', $select);
+
+				if($optgroups.size()){
+
+					$optgroups.each(function(){
+						var $optgroup = $(this);
+						var $group = $('<div>').addClass('group');
+						var label = $optgroup.attr('label');
+
+						$parent = $group;
+						$group.appendTo($dropdown);
+
+						if($optgroup.is(':disabled')){
+							$group.addClass('disabled');
+						}
+
+						if(label){
+							var $span = $('<span>');
+
+							$span.text(label);
+							$group.append($span)
+						}
+
+						($.proxy(new_items, this))();
+					});
+				}
+
+				else{
+					$parent = $dropdown;
+					($.proxy(new_items, $select))();
+				}
+
+				$options = $('option', $select);
+				$items = $('li', $dropdown);
+
+				$items.click(function(){
+
+					var $item = $(this);
+
+					if(!$item.data('disabled')){
+						$options.eq($items.index(this)).prop('selected', true);
+						$select.change();
+						$document.mousedown().mouseup();
+					}
+				});
+
+				$dropdown.wrapInner('<div>');
+
+				var placeholder = $select.data('placeholder');
+
+				/* Вставляем текст плейсхолдера, если он задан, и у option нет атрибута selected */
+				if(!$selected_option.filter('[selected]').length && placeholder){
+					$span.html(placeholder).addClass('placeholder');
+				}
+
+				else{
+					$span.html($selected_item.html());
+				}
+
+				if(configuration.set_width){
+					$select.trigger('jselector:set_width');
+				}
+			});
+
+			$select.trigger('jselector:update');
 
 			$select.change(function(){
-				var i = $('option:selected', this).index();
 
-				$item = $('li', $ul).removeClass('selected').eq(i);
-
-				$item.addClass('selected');
-				$span.text($item.text());
+				var i = $options.index($options.filter(':selected'));
+				$selected_item = $items.removeClass('selected').eq(i).addClass('selected');
+				$span.html($selected_item.html()).removeClass('placeholder');
 			});
 
-			$selector_wrap.click(function(e){
-				if(!$ul.is(':visible')){
-					$ul.show();
+			$wrap.mousedown(function(e){
+				if(e.which == 1){	
+					if(!shown){
+						$document.one('mouseup', function(e){
+							$select.trigger('jselector:show');
 
-					$(document).click();
-					
-					setTimeout(function(){
-						$(document).one('click.jselector', function(){
-							$ul.hide();
+							setTimeout(function(){
+								$document.mousedown(function(e){
+									if(!$(e.target).parents().is($dropdown)){
+										
+										e.preventDefault();
+
+										$document.one('mouseup', function(){
+											$select.trigger('jselector:hide');
+										});
+
+										$document.unbind(e);
+									}
+								});
+							});
 						});
-					});
+					}
 				}
 			});
 		};
